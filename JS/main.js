0263634a40f9d945739ca2c9c7977df2d305cc48
Luke Beatty <lukebeatty@Lukes-MBP.fc.ca>
2019-01-21 15:47:31 -0500

    adding interactive badge

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+(() => {
+	// elf-invoking anonymous fuctions (or lambda)
+	// var => means creat a variable
+	// what ever cmes after the = is what the variable
+	var	images = document.querySelectorAll('img');
+
+	// document is always the webpage
+	// after the dot is always a method or a property
+
+	console.log(images);
+})();
